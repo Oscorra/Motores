1e24cf940f6d405e6b26ecc10922679e1f60b286
Oscorra <afkoscorra@gmail.com>
2026-02-27 12:56:45 +0100

    Player movement & camera

---
+++ Motores/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private MeshRenderer m_Renderer_Jugador;
+    private Rigidbody rb_Jugador;
+
+    public float multiplicadorDesplazamiento = 8.0f;
+    public Transform cameraTransform;
+    public float inputX { get; private set; }
+    public float inputZ { get; private set; }
+    private Vector3 direccionMovimiento;
+
+    void Start()
+    {
+        m_Renderer_Jugador = GetComponent<MeshRenderer>();
+        rb_Jugador = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        Vector3 camForward = cameraTransform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = cameraTransform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        direccionMovimiento = (camForward * inputZ + camRight * inputX);
+    }
+
+    void FixedUpdate()
+    {
+        if (direccionMovimiento.sqrMagnitude > 0.1)
+        {
+            direccionMovimiento.Normalize();
+            Quaternion rotacionObjetivo = Quaternion.LookRotation(direccionMovimiento, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionObjetivo, 0.2f);
+
+            Vector3 direccion = rb_Jugador.position + direccionMovimiento * multiplicadorDesplazamiento * Time.fixedDeltaTime;
+            rb_Jugador.MovePosition(direccion);
+        }
+
+        Vector3 movimientoLocal = transform.InverseTransformDirection(direccionMovimiento);
+    }
+}
